3b6bb29581f20ab0f9c62c1f41cc1a0d1d65bed8
Jason Crawford <jcrawford@dm78.org>
2020-12-12 10:12:31 -0800

    Move s3m pattern data into its own file

--- internal/format/s3m/s3m.go
+++ internal/format/s3m/s3m.go
@@ -2,7 +2,6 @@
 package s3m
 
 import (
-	"gotracker/internal/format/s3m/channel"
 	"gotracker/internal/format/s3m/util"
 	"gotracker/internal/player/intf"
 )
@@ -133,43 +132,6 @@ type Header struct {
 	InstrumentPointers []ParaPointer
 	PatternPointers    []ParaPointer
 	Panning            [32]PanningFlags
-}
-
-// RowData is the data for each row
-type RowData struct {
-	intf.Row
-	Channels [32]channel.Data
-}
-
-// GetChannels returns an interface to all the channels in the row
-func (r RowData) GetChannels() []intf.ChannelData {
-	c := make([]intf.ChannelData, len(r.Channels))
-	for i := range r.Channels {
-		c[i] = &r.Channels[i]
-	}
-
-	return c
-}
-
-// Pattern is the data for each pattern
-type Pattern struct {
-	intf.Pattern
-	Packed PackedPattern
-	Rows   [64]RowData
-}
-
-// GetRow returns the interface to the row at index `row`
-func (p Pattern) GetRow(row uint8) intf.Row {
-	return &p.Rows[row]
-}
-
-// GetRows returns the interfaces to all the rows in the pattern
-func (p Pattern) GetRows() []intf.Row {
-	rows := make([]intf.Row, len(p.Rows))
-	for i, pr := range p.Rows {
-		rows[i] = pr
-	}
-	return rows
 }
 
 // Song is the full definition of the song data of an Song file

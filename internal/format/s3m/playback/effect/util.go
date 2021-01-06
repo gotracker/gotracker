b008738d27a33f954d5aa97aeca7c3249eacbde8
Jason Crawford <jcrawford@dm78.org>
2021-01-05 18:39:37 -0800

    Further support for unhandled effects.

--- internal/format/s3m/playback/effect/util.go
+++ internal/format/s3m/playback/effect/util.go
@@ -1,6 +1,7 @@
 package effect
 
 import (
+	"fmt"
 	"math"
 	"math/rand"
 
@@ -12,6 +13,34 @@ import (
 	"gotracker/internal/player/note"
 	"gotracker/internal/player/oscillator"
 )
+
+// UnhandledCommand is an unhandled command
+type UnhandledCommand struct {
+	intf.Effect
+	Command uint8
+	Info    uint8
+}
+
+// PreStart triggers when the effect enters onto the channel state
+func (e UnhandledCommand) PreStart(cs intf.Channel, p intf.Playback) {
+	panic("unhandled command")
+}
+
+// Start triggers on the first tick, but before the Tick() function is called
+func (e UnhandledCommand) Start(cs intf.Channel, p intf.Playback) {
+}
+
+// Tick is called on every tick
+func (e UnhandledCommand) Tick(cs intf.Channel, p intf.Playback, currentTick int) {
+}
+
+// Stop is called on the last tick of the row, but after the Tick() function is called
+func (e UnhandledCommand) Stop(cs intf.Channel, p intf.Playback, lastTick int) {
+}
+
+func (e UnhandledCommand) String() string {
+	return fmt.Sprintf("%c%0.2x", e.Command+'@', e.Info)
+}
 
 func doVolSlide(cs intf.Channel, delta float32, multiplier float32) {
 	av := cs.GetActiveVolume()

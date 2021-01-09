fa1669485ca2e02a35b34384ffb72e91c27310bf
Jason Crawford <jcrawford@dm78.org>
2021-01-08 23:55:24 -0800

    adding export comment

--- internal/format/xm/playback/util/util.go
+++ internal/format/xm/playback/util/util.go
@@ -115,6 +115,7 @@ func PanningFromXm(pos uint8) panning.Position {
 	return panning.MakeStereoPosition(float32(pos), 0, 0xFF)
 }
 
+// PanningToXm returns the xm panning value for a radian panning position
 func PanningToXm(pan panning.Position) uint8 {
 	return uint8(panning.FromStereoPosition(pan, 0, 0xFF))
 }

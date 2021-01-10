5bfd3249f2c48576f521af4570a84d8a83c5cdd7
Jason Crawford <jcrawford@dm78.org>
2021-01-09 17:54:21 -0800

    Move common code for protracker sine to utils

--- internal/format/s3m/layout/channel/oscillator.go
+++ internal/format/s3m/layout/channel/oscillator.go
@@ -2,6 +2,8 @@ package channel
 
 import (
 	"math/rand"
+
+	formatutil "gotracker/internal/format/internal/util"
 )
 
 // WaveTableSelect is the selection code for which waveform to use in an oscillator
@@ -24,40 +26,23 @@ type Oscillator struct {
 	Pos   int8
 }
 
-var (
-	protrackerSineTable = [...]uint8{
-		0, 24, 49, 74, 97, 120, 141, 161,
-		180, 197, 212, 224, 235, 244, 250, 253,
-		255, 253, 250, 244, 235, 224, 212, 197,
-		180, 161, 141, 120, 97, 74, 49, 24,
-	}
-)
-
-func getProtrackerSine(pos int) float32 {
-	sin := float32(protrackerSineTable[pos&0x1f]) / 255
-	if pos > 32 {
-		return -sin
-	}
-	return sin
-}
-
 // GetWave returns the wave amplitude for the current position
 func (o *Oscillator) GetWave(depth float32) float32 {
 	var vib float32
 	switch o.Table {
 	case WaveTableSelectSine:
-		vib = getProtrackerSine(int(o.Pos))
+		vib = formatutil.GetProtrackerSine(int(o.Pos))
 	case WaveTableSelectSawtooth:
-		vib = (32.0 - float32(o.Pos&64)) / 32.0
+		vib = (32.0 - float32(o.Pos&0x3f)) / 32.0
 	case WaveTableSelectSquare:
-		v := getProtrackerSine(int(o.Pos))
+		v := formatutil.GetProtrackerSine(int(o.Pos))
 		if v > 0 {
 			vib = 1.0
 		} else {
 			vib = -1.0
 		}
 	case WaveTableSelectRandom:
-		vib = getProtrackerSine(rand.Int() & 0x3f)
+		vib = formatutil.GetProtrackerSine(rand.Int() & 0x3f)
 	}
 	delta := vib * depth
 	return delta

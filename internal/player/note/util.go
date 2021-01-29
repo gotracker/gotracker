183e1ae42073f8988cb972afd8b6ff9f301a7d26
Jason Crawford <jcrawford@dm78.org>
2021-01-28 19:51:36 -0800

    Fix for slightly off-key instruments - non-power-of-2 C2(C5) speeds be damned

--- internal/player/note/util.go
+++ internal/player/note/util.go
@@ -3,7 +3,7 @@ package note
 import "fmt"
 
 // C2SPD defines the C-2 (or in some players cases C-4) note sampling rate
-type C2SPD uint16
+type C2SPD float32
 
 // Finetune is a 1/64th of a Semitone
 type Finetune int16

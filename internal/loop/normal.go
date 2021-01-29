e2f8828c1f5ea540cceeff03cb87977a7c9e898d
Jason Crawford <jcrawford@dm78.org>
2021-01-28 19:54:29 -0800

    Renaming loop mode internal calcs to match reality

--- internal/loop/normal.go
+++ internal/loop/normal.go
@@ -1,23 +1,27 @@
 package loop
 
-func calcLoopPosMode1(pos int, length int, loopBegin int, loopEnd int) (int, bool) {
-	//  |start>----------------------------------------end| <= on playthrough 1, whole sample plays
+func calcLoopPosNormal(pos int, length int, loopBegin int, loopEnd int) (int, bool) {
+	//  |start>-----------------------loopEnd|------------| <= on playthrough 1, whole sample plays
 	//  |-------------|loopBegin>-----loopEnd|------------| <= only if looped and on playthrough 2+, only the part that loops plays
 	//  |-------------|loopBegin>----------------------end| <= on playthrough 2+, the loop ends and playback continues to end, if !keyOn
-	switch {
-	case pos < 0:
+	if pos < 0 {
 		return 0, false
-	case pos < length:
+	}
+	if pos < loopEnd {
 		return pos, false
 	}
 
 	loopLen := calcLoopLen(loopBegin, loopEnd)
 	if loopLen < 0 {
+		if pos < length {
+			return pos, false
+		}
 		return length, false
 	} else if loopLen == 0 {
 		return loopBegin, true
 	}
 
-	loopedPos := (pos - length) % loopLen
+	dist := pos - loopEnd
+	loopedPos := dist % loopLen
 	return loopBegin + loopedPos, true
 }

951a601d4c0242f13a931327795048f34c28a042
Jason Crawford <jcrawford@dm78.org>
2021-02-15 10:59:21 -0800

    Fix for missing output filter

--- internal/voice/internal/component/sampler.go
+++ internal/voice/internal/component/sampler.go
@@ -10,11 +10,12 @@ import (
 
 // Sampler is a sampler component
 type Sampler struct {
-	sample      pcm.Sample
-	pos         sampling.Pos
-	keyOn       bool
-	wholeLoop   loop.Loop
-	sustainLoop loop.Loop
+	sample       pcm.Sample
+	pos          sampling.Pos
+	keyOn        bool
+	loopsEnabled bool
+	wholeLoop    loop.Loop
+	sustainLoop  loop.Loop
 }
 
 // Setup sets up the sampler
@@ -34,17 +35,31 @@ func (s *Sampler) GetPos() sampling.Pos {
 	return s.pos
 }
 
-// SetKeyOn sets the key-on value (for loop processing)
-func (s *Sampler) SetKeyOn(on bool) {
-	s.keyOn = on
+// Attack sets the key-on value (for loop processing)
+func (s *Sampler) Attack() {
+	s.keyOn = true
+	s.loopsEnabled = true
+}
+
+// Release releases the key-on value (for loop processing)
+func (s *Sampler) Release() {
+	s.keyOn = false
+}
+
+// Fadeout disables the loops (for loop processing)
+func (s *Sampler) Fadeout() {
+	s.loopsEnabled = false
 }
 
 // GetSample returns a multi-channel sample at the specified position
 func (s *Sampler) GetSample(pos sampling.Pos) volume.Matrix {
 	v0 := s.getConvertedSample(pos.Pos)
-	if len(v0) == 0 && ((s.keyOn && s.sustainLoop.Enabled()) || s.wholeLoop.Enabled()) {
-		v01 := s.getConvertedSample(pos.Pos)
-		panic(v01)
+	if len(v0) == 0 {
+		if s.canLoop() {
+			v01 := s.getConvertedSample(pos.Pos)
+			panic(v01)
+		}
+		return v0
 	}
 	if pos.Frac == 0 {
 		return v0
@@ -56,11 +71,26 @@ func (s *Sampler) GetSample(pos sampling.Pos) volume.Matrix {
 	return v0
 }
 
+func (s *Sampler) canLoop() bool {
+	switch {
+	case !s.loopsEnabled:
+		return false
+	case s.keyOn && s.sustainLoop.Enabled():
+		return true
+	case s.wholeLoop.Enabled():
+		return true
+	}
+	return false
+}
+
 func (s *Sampler) getConvertedSample(pos int) volume.Matrix {
 	if s.sample == nil {
 		return volume.Matrix{}
 	}
 	sl := s.sample.Length()
+	if pos >= sl && !s.canLoop() {
+		return volume.Matrix{}
+	}
 	pos, _ = loop.CalcLoopPos(s.wholeLoop, s.sustainLoop, pos, sl, s.keyOn)
 	if pos < 0 || pos >= sl {
 		return volume.Matrix{}
